944fb7ddb5172826abd6d22735952bef66de5dec
elcapoanonimo1 <72955065+elcapoanonimo1@users.noreply.github.com>
2021-03-22 23:18:47 -0300

    Update jsbloqued.js

--- jsbloqued.js
+++ jsbloqued.js
@@ -1,1 +1,17 @@
-		jQuery(window).resize(function(){cr_sizeCanvas(jQuery(window).width(),jQuery(window).height());});window.addEventListener('orientationchange',cr_sizeCanvas,false);jQuery(document).ready(function() {cr.createRuntime("c2canvas");cr_sizeCanvas(jQuery(window).width(),jQuery(window).height());});function onVisibilityChanged(){if(document.hidden||document.mozHidden||document.webkitHidden||document.msHidden) cr_setSuspended(true);else cr_setSuspended(false);} document.addEventListener("visibilitychange",onVisibilityChanged,false);document.addEventListener("mozvisibilitychange",onVisibilityChanged,false);document.addEventListener("webkitvisibilitychange",onVisibilityChanged,false);document.addEventListener("msvisibilitychange",onVisibilityChanged,false);
+jQuery(window).resize(function () {
+	cr_sizeCanvas(jQuery(window).width(), jQuery(window).height());
+});
+window.addEventListener('orientationchange', cr_sizeCanvas, false);
+jQuery(document).ready(function () {
+	cr.createRuntime("c2canvas");
+	cr_sizeCanvas(jQuery(window).width(), jQuery(window).height());
+});
+
+function onVisibilityChanged() {
+	if (document.hidden || document.mozHidden || document.webkitHidden || document.msHidden) cr_setSuspended(true);
+	else cr_setSuspended(false);
+}
+document.addEventListener("visibilitychange", onVisibilityChanged, false);
+document.addEventListener("mozvisibilitychange", onVisibilityChanged, false);
+document.addEventListener("webkitvisibilitychange", onVisibilityChanged, false);
+document.addEventListener("msvisibilitychange", onVisibilityChanged, false);
